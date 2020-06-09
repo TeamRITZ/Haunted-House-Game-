2c06bdcb0d53fd8141832d5b15ad62a55a92bb80
Bluestopsign98 <jamesmele01@hotmail.com>
2020-06-09 16:17:42 -0400

    Revert "Merge branch 'master' of https://github.com/TeamRITZ/Haunted-House-Game-"
    This reverts commit 65c44ce870022be95510a26e39423b9f02f685ec, reversing
    changes made to 07654042eb6059e34a4f0ad28be70306683b0c0b.

--- Player.gd
+++ Player.gd
@@ -2,11 +2,8 @@ extends Area2D
 
 export var battery = 100;
 export var health = 100;
-export var speed = 400
-export var battery_drain = 0.5
-export var battery_recharge = 0.25
 var flashlight_enabled = true
-
+export var speed = 400
 signal health_changed
 var screen_size
 # Declare member variables here. Examples:
@@ -20,7 +17,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	#movement on arrow keys
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -31,16 +27,13 @@ func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 
-	#flashlight on space bar
 	if Input.is_action_pressed(("ui_accept")):
 		if flashlight_enabled:
 			$Light2D.enabled = true
 			$FlashlightBeam/CollisionShape2D.disabled = false
-	#turns off flashlight when key released
 	else:
 		$Light2D.enabled = false
 		$FlashlightBeam/CollisionShape2D.disabled = true
-	#makes sure diagonal movement is the same speed as horizontal or vertical
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -51,16 +44,13 @@ func _process(delta):
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
-	#flashlight battery drain, recharge
+	
 	if $Light2D.enabled:
-		battery -= battery_drain
+		battery -= .5
 	else:
-		battery += battery_recharge
-	#Keeps battery between 0 and 100
+		battery += .25
 	battery = clamp(battery, 0, 100)
 	
-	#if battery hits zero, turns off the light, disables flashlight until
-	# recharged to at least 30
 	if battery == 0:
 		$Light2D.enabled = false
 		flashlight_enabled = false
@@ -68,7 +58,6 @@ func _process(delta):
 	if battery > 30:
 		flashlight_enabled = true
 		
-	# handles sprite and flashlight rotation based on movement direction
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "side"
 		$LightOccluder2D.rotation_degrees = 0
@@ -88,17 +77,16 @@ func _process(delta):
 
 	
 
-# player taks 25 hp damage on enemy contact. Color turns red, hit detection
-# temporarily turned off
+
 func _on_Player_area_entered(area):
-	if area.get_name() != "FlashlightBeam": # don't want the flashlight to hurt us
+	if area.get_name() != "FlashlightBeam":
 		health -= 25
 		$CollisionShape2D.set_deferred("disabled", true)
 		$HurtTimer.start()
 		$AnimatedSprite.set_modulate(Color(1,0,0))
 		emit_signal("health_changed", health)
 
-# Restores hit detection and normal color
+
 func _on_HurtTimer_timeout():
 	$CollisionShape2D.disabled = false
 	$AnimatedSprite.set_modulate(Color(1,1,1))
