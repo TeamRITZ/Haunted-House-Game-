6f26fdce9a5c071a9843028ca2686593ac54134c
ZachBearse <zachbearse@gmail.com>
2020-06-13 15:00:47 -0400

    2D Camera, Scene changer, small edits

---
+++ SCENES/Scripts/Camera2D.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+
+func _process(delta):
+	var pos = get_node("../Player").global_position
+	var x = floor(pos.x / screen_size.x) * screen_size.x
+	var y = floor(pos.y / screen_size.y) * screen_size.y
+	global_position = lerp(global_position, Vector2(x,y), 0.1)
