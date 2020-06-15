2e87800e730ff0da682db151b89ce44034632845
ZachBearse <zachbearse@gmail.com>
2020-06-15 18:10:29 -0400

    JournalPage added

--- JournalPage.gd
+++ JournalPage.gd
@@ -1,25 +1,29 @@
-extends Control
+extends CanvasLayer
 
-var text = "The candles in this room are strange.\n Those that are lit refuse to be extinguished. \n The others refuse to stay lit."
+signal finished()
+#var text = "The candles in this room are strange.\n Those that are lit refuse to be extinguished. \n The others refuse to stay lit."
 
 var finished = false
-var line = 1
+
 func _ready():
-	$AnimatedSprite.visible = false
-	$RichTextLabel.bbcode_text = text
-	$RichTextLabel.percent_visible = 0
+	$JournalPage.visible = false
+	$JournalPage/AnimatedSprite.visible = false
+	$JournalPage/RichTextLabel.percent_visible = 0
 	#load_dialog()
 func _process(_delta):
 	if finished and Input.is_action_just_pressed("interact"):
+		emit_signal("finished")
 		get_tree().paused = false
-		visible = false
+		$JournalPage.visible = false
 		finished = false
-func load_dialog():
+func load_dialog(text):
 	get_tree().paused = true
-	$Tween.interpolate_property($RichTextLabel, "percent_visible", 0, 1, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
-	yield($Tween, "tween_completed")
-	$AnimatedSprite.visible = true
-	$AnimatedSprite.play()
+	$JournalPage.visible = true
+	$JournalPage/RichTextLabel.bbcode_text = text
+	$JournalPage/Tween.interpolate_property($JournalPage/RichTextLabel, "percent_visible", 0, 1, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$JournalPage/Tween.start()
+	yield($JournalPage/Tween,"tween_completed")
+	$JournalPage/AnimatedSprite.visible = true
+	$JournalPage/AnimatedSprite.play()
 	finished = true
-	$AnimatedSprite.visible = true
+	$JournalPage/AnimatedSprite.visible = true
--- Paper.gd
+++ Paper.gd
@@ -1,14 +1,22 @@
 extends Area2D
 
-
+var readable = true
+var text = "The candles in this room are strange.\n Those that are lit refuse to be extinguished. \n The others refuse to stay lit."
 # Called when the node enters the scene tree for the first time.
 #func _ready():
-	#$JournalPage.visible = false
 
 func interact_action(_area):
-	$JournalPage.visible = true
-	$JournalPage/JournalPage.visible = true
-	$JournalPage/JournalPage.load_dialog()
+	if readable:
+		JournalPage.load_dialog(text)
+		#The following is an over-complicated way to make sure
+		#the player doesn't automatically read the note again
+		readable = false #mark unreadable
+		yield(JournalPage, "finished") #wait for the player to finish reading
+		yield(get_tree().create_timer(.5), "timeout") #wait half a second so pressing E doesn't trigger the read again
+		readable = true
+	else:
+		return
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
