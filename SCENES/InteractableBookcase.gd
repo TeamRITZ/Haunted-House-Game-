78b35cd0806a00bac962f8feda1ebfcc6ba184d5
Bluestopsign98 <jamesmele01@hotmail.com>
2020-06-17 10:01:33 -0400

    puzzle in library now gives player the silver key

--- SCENES/InteractableBookcase.gd
+++ SCENES/InteractableBookcase.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 var secretFound = false
 
+func _ready():
+	$silverKey/CollisionShape2D.disabled = true
+	$silverKey/Sprite.visible = false
+
 func interact_action(area):
 	if area.get_parent().hasAnatomyBook == true:
 		if secretFound == false:
 			$secretSound.play()
 			secretFound = true
+			$silverKey/CollisionShape2D.disabled = false
+			$silverKey/Sprite.visible = true
